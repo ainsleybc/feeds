45f16e8f4ff0626a8000afd07738b53ac24dd654
Ainsley <ainsley.chang@ovoenergy.com>
2020-11-01 13:44:15 +0000

    Adds prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,40 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    'quotes': ['error', 'single', { 'avoidEscape': true }],
-    'semi': ['error', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
     'no-empty': 'error',
-    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
     'max-len': [
       'error',
       {
-        'code': 120,
-        'ignoreStrings': true,
-        'ignoreRegExpLiterals': true
-      }
+        code: 120,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+      },
     ],
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'import/extensions': ['error', 'never', { 'json': 'always' }],
+    'import/extensions': ['error', 'never', { json: 'always' }],
     'import/order': [
       'error',
       {
-        'alphabetize': {
-          'order': 'asc',
-          'caseInsensitive': true,
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
         'newlines-between': 'always-and-inside-groups',
-        'groups': [
-          ['builtin', 'external'],
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object'
-        ]
-      }
+        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index', 'object'],
+      },
     ],
   },
   ignorePatterns: ['node_modules/', 'dist/'],
