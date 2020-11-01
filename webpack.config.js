913ecd8879734a873a6b25253367064258035f90
Ainsley <ainsley.chang@ovoenergy.com>
2020-11-01 01:01:05 +0000

    Adds typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
